86eb7eb7b27272cc06210215f8844ce9f16d91f4
Shivangi Nagpal <shivangi.nagpal@sjsu.edu>
2020-05-12 21:26:30 +0000

    Remove credentials

--- backend/config.js
+++ backend/config.js
@@ -1,5 +1,5 @@
 const config = {
-    mongoDB: 'mongodb+srv://shivangi12:shivangi12@cluster0-kybdh.mongodb.net/test?retryWrites=true&w=majority',
+    mongoDB: 'mongodb+srv://<username>:<password>@cluster0-kybdh.mongodb.net/test?retryWrites=true&w=majority',
     secret: 'Life ain\'t Easy'
 };
 
